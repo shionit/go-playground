1177233d62a3edbef867b48cd9d2e14b0b0bed1f
shionit <frontier.engine@gmail.com>
2021-08-17 23:00:43 +0900

    study go forward

---
+++ src/handson/omikujiweb/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	omikuji := []string{
+		"大大吉",
+		"大吉",
+		"中吉",
+		"小吉",
+		"末吉",
+		"吉",
+		"凶",
+		"大凶",
+	}
+	fmt.Fprint(w, "あなたの運勢は、"+omikuji[rand.Intn(len(omikuji))])
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
